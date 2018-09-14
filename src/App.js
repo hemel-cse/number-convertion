939770c52499f906d0fe683036ab8819a5637ed9
Hasibul Amin Hemel <hasibul.amin.hemel@gmail.com>
2018-09-15 00:15:13 +0600

    frontend

--- src/App.js
+++ src/App.js
@@ -1,19 +1,89 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Button, Layout, Row, Col, Card, Input } from 'antd';
 import './App.css';
 
+const { Header, Footer, Content } = Layout;
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.handleChangeBinToDec = this.handleChangeBinToDec.bind(this);
+    this.handleChangeBinToDecInp = this.handleChangeBinToDecInp.bind(this);
+    this.handleChangeBinToOct = this.handleChangeBinToOct.bind(this);
+
+    this.state = {
+      BinToDec: "",
+      BinToDecRes: "",
+      BinToOct: "",
+      BinToOctRes: "",
+
+    };
+  
+  }
+
+  handleChangeBinToDecInp(e) {
+    e.preventDefault();
+
+    this.setState({BinToDec: e.target.value});
+
+  }
+
+  handleChangeBinToDec() {
+    
+    this.setState({
+      BinToDecRes: "ok"
+    });
+  }
+
+  handleChangeBinToOctInp(e) {
+    e.preventDefault();
+
+    this.setState({BinToOct: e.target.value});
+
+  }
+
+  handleChangeBinToOct() {
+    
+    this.setState({
+      BinToOctRes: "ok"
+    });
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Layout>
+        <Header className="App-header">Number Conversion</Header>
+        <Layout>
+          <Content>
+            <Row gutter={8} justify="center">
+              <Col span={12}>
+                <div className="App-card">
+                  <Card title="Binary to Decimal">
+                    <Input 
+                      defaultValue="1010"
+                      value={this.state.BinToDec} 
+                      onChange={this.handleChangeBinToDecInp}
+                      onPressEnter={this.handleChangeBinToDec}  
+                      /> 
+                    <br/>
+                    <Button type="primary">Convert</Button>
+                  </Card>
+                </div>
+              </Col>
+              <Col span={12}>
+                <div className="App-card">
+                  <Card title="Binary to Octal">
+                    <Button type="primary">Convert</Button>
+                  </Card>
+                </div>
+              </Col>
+            </Row>
+          </Content>
+        </Layout>
+        <Footer className="App-footer">Copyright - BUBT</Footer>
+      </Layout>
     );
   }
 }
